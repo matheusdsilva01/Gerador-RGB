6d5bc6a92647ea9f477adef5d01409897073760c
Matheus Silva <ms25022003@gmail.com>
2022-06-27 11:43:20 -0300

    add resposive and theme dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Pencil from './assets/pencil.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
@@ -7,31 +7,35 @@ const App = () => {
   const [colorGreen, setColorGreen] = useState<string>('121');
   const [colorBlue, setColorBlue] = useState<string>('184');
 
+  const date = new Date();
+
   return (
     <>
-      <h1>Gerador RGB</h1>
-      <section className="datetime">
-        <h3><img src={Pencil} alt="caneta" /> {new Date().toLocaleDateString()} - {`${new Date().getHours()}:${(new Date().getMinutes() < 10 ? '0' : '') + new Date().getMinutes()}`}</h3>
-      </section>
-      <section className="container commands">
-        <section className="inputs-ranger">
-          <div className="input-ranger red">
-            <input type="range" min="0" max="255" name="red" id="red" onChange={(e) => setColorRed(e.target.value)} />
-            <h1>red: {colorRed}</h1>
-          </div>
-          <div className="input-ranger green">
-            <input type="range" min="0" max="255" name="green" id="green" onChange={(e) => setColorGreen(e.target.value)} />
-            <h1>green: {colorGreen}</h1>
-          </div>
-          <div className="input-ranger blue">
-            <input type="range" min="0" max="255" name="blue" id="blue" onChange={(e) => setColorBlue(e.target.value)} />
-            <h1>blue: {colorBlue}</h1>
-          </div>
+      <div className={`container-app ${date.getHours() >= 18 && date.getHours() < 6 ? 'dark' : ''}`}>
+        <h1>RGB Generator</h1>
+        <section className="datetime">
+          <h3><img src={Pencil} alt="caneta" /> {date.toLocaleDateString()} - {`${date.getHours()}:${(date.getMinutes() < 10 ? '0' : '') + date.getMinutes()}`}</h3>
         </section>
-        <div id="color" style={{ backgroundColor: `rgb(${colorRed}, ${colorGreen}, ${colorBlue})` }}></div>
-        <p>RGB: {`(${colorRed}, ${colorGreen}, ${colorBlue})`}</p>
-      </section>
-      <h4>Desenvolvido por <a href="https://matheusdsilva01.github.io/portfolio/" target="_blank" rel="noopener noreferrer">Matheus Silva &#128150;</a></h4>
+        <section className="container commands">
+          <section className="inputs-ranger">
+            <div className="input-ranger red">
+              <input type="range" min="0" max="255" name="red" id="red" onChange={(e) => setColorRed(e.target.value)} />
+              <h4>red: {colorRed}</h4>
+            </div>
+            <div className="input-ranger green">
+              <input type="range" min="0" max="255" name="green" id="green" onChange={(e) => setColorGreen(e.target.value)} />
+              <h4>green: {colorGreen}</h4>
+            </div>
+            <div className="input-ranger blue">
+              <input type="range" min="0" max="255" name="blue" id="blue" onChange={(e) => setColorBlue(e.target.value)} />
+              <h4>blue: {colorBlue}</h4>
+            </div>
+          </section>
+          <div id="color" style={{ backgroundColor: `rgb(${colorRed}, ${colorGreen}, ${colorBlue})` }}></div>
+          <p>RGB: {`(${colorRed}, ${colorGreen}, ${colorBlue})`}</p>
+        </section>
+        <h4>Developed by <a href="https://matheusdsilva01.github.io/portfolio/" target="_blank" rel="noopener noreferrer">Matheus Silva &#128150;</a></h4>
+      </div>
     </>
   )
 }
